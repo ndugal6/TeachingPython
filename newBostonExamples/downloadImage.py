d70afbe8c5b2e708ac5791425fc3eb37102316b2
Nick <ndugal1@lsu.edu>
2016-11-17 04:35:10 -0600

    Moved files to folder for ease

--- newBostonExamples/downloadImage.py
+++ newBostonExamples/downloadImage.py
@@ -7,4 +7,6 @@ def download_web_image(url):
     urllib.request.urlretrieve(url, full_name)
 
 #Downloads and saves the image as a jpg into the working directory
-download_web_image("https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/13680571_10207767142509537_4408524643039459221_n.jpg?oh=a5bc08f55654546a68ce2e4dcfacb73c&oe=58900B18")
+download_web_image("https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/"
+                   "13680571_10207767142509537_4408524643039459221_n"
+                   ".jpg?oh=a5bc08f55654546a68ce2e4dcfacb73c&oe=58900B18")
